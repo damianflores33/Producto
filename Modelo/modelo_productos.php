e1e21b433f14827ce67f242f2398c1511037d510
Keibert64 <keibert.fuentes@gmail.com>
2024-05-24 18:05:33 -0400

    Keibert

--- Modelo/modelo_productos.php
+++ Modelo/modelo_productos.php
@@ -14,6 +14,21 @@ function concultarCategorias(){
     return($resul);
 }
 
+function concultarProductosPorCategorias($categoria){
+    global $conn;
+    $sql = "SELECT nombre_producto, imagen_principal, precio_venta_1";
+    $sql .= " FROM producto";
+    $sql .= " WHERE 1 = 1";
+    if (strlen($categoria) > 0)
+        $sql .= " AND categoria = $categoria";
+    $sql .= " ORDER BY 1;";
+    // echo $sql;
+    $num = 0;
+    $resul = consultar_db($num, $sql, $conn);
+    
+    return($resul);
+}
+
 function InsertarNuevoProducto($nombre_producto,
                                 $costo,
                                 $precio_venta_1,
@@ -25,7 +40,7 @@ function InsertarNuevoProducto($nombre_producto,
 ){
     global $conn;
     $msj = "";
-    $cod_producto = $categoria . "-" . $imagen_principal;
+    $cod_producto = $categoria . "-" . trim($imagen_principal, '.jpg');
     $sql = "INSERT INTO producto(cod_producto, nombre_producto, costo, precio_venta_1, precio_venta_2, stock_minimo, stock_actual, imagen_principal, categoria)";
     $sql .=  " VALUES (";
     $sql .= "'" . $cod_producto . "', ";
---
+++ Vista/galeria.php
@@ -0,0 +1,123 @@
+<?php 
+// Diplomado en Programación Web
+// Autor Damián Flores
+// Catalogo de Productos
+
+require_once("../Controlador/procesar_producto.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <title>Shop</title>
+</head>
+<body>
+    <header>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="../index.php">
+              <img src="../imagenes/logoHome.png" alt="Shop" width="30" height="24">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <a class="nav-link" href="productos.php">Productos</a>
+                <a class="nav-link" href="galeria.php">Galeria</a>
+              </div>
+            </div>
+          </div>
+        </nav>
+    </header>
+
+    <main>
+      <div class="d-flex justify-content-center">
+      
+        <div class="container text-center">
+            <div class="row">
+              <div class="col text-end">&nbsp;</div>
+              <div class="col text-end">&nbsp;</div>
+              
+              <div class="col text-end">
+                <form action="galeria.php" method="post" enctype="multipart/form-data">
+                  <input type="hidden" id="accion" name="accion" value="NUEVO PRODUCTO">
+                    <label for="categoria" class="form-label">Categor&iacute;a:</label>
+                      <?php
+                        echo selectCategoria();
+                      ?>
+                </form>
+              </div>
+            </div>  
+
+           <div class="row justify-content-between">
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../Imagenes/productos/664d3907a5118.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="./vista/productos.html" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                      </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../Imagenes/productos/664d3907a5118.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="./vista/productos.html" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                      </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../Imagenes/productos/664d3907a5118.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="./vista/productos.html" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                      </div>
+                </div>
+              </div>
+              </div>
+          </div>
+      </div>
+    </main>
+  <br><br>
+  ------------------------
+<div id="respuestaccc">
+
+</div>
+---------------------------
+<!-- ----------------------------------------------------------------------- -->
+<!-- ----------------------------------------------------------------------- -->
+<!-- ----------------------------------------------------------------------- -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#categoria').change(function() {
+                var categoria1 = $(this).val();
+
+                $.ajax({
+                    url: '../Controlador/procesarSeleccion.php',
+                    type: 'POST',
+                    data: { categorian: categoria1 },
+                    dataType: 'json',
+                    success: function(respuesta) {
+                        $('#respuestaccc').html(respuesta.informacion);
+                    }
+                });
+            });
+        });
+    </script>
+
+</body>
+</html>
