36402ccb0012be3f11198be3eab04a6ba6ff0291
DamianFlores <damian.flores_ext@simetrik.com>
2024-05-25 09:02:10 -0400

    cambios

--- Vista/galeria.php
+++ Vista/galeria.php
@@ -52,40 +52,43 @@ require_once("../Controlador/procesar_producto.php");
                 </form>
               </div>
             </div>  
+<div id="respuesta">
+<div class="row justify-content-between">
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../Imagenes/productos/664d3907a5118.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="./vista/productos.html" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                      </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../Imagenes/productos/664d3907a5118.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="./vista/productos.html" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                      </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../Imagenes/productos/664d3907a5118.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="./vista/productos.html" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                      </div>
+                </div>
+              </div>
+              </div>
 
-           <div class="row justify-content-between">
-                <div class="col">
-                    <div class="card" style="width: 18rem;">
-                        <img src="../Imagenes/productos/664d3907a5118.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <h5 class="card-title">Card title</h5>
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                          <a href="./vista/productos.html" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                      </div>
-                </div>
-                <div class="col">
-                    <div class="card" style="width: 18rem;">
-                        <img src="../Imagenes/productos/664d3907a5118.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <h5 class="card-title">Card title</h5>
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                          <a href="./vista/productos.html" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                      </div>
-                </div>
-                <div class="col">
-                    <div class="card" style="width: 18rem;">
-                        <img src="../Imagenes/productos/664d3907a5118.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <h5 class="card-title">Card title</h5>
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                          <a href="./vista/productos.html" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                      </div>
-                </div>
-              </div>
-              </div>
+</div>
+           
           </div>
       </div>
     </main>
